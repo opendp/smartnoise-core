30addb16b9e6190a5b4efae47b031183618912b2
Shoeboxam <shoeboxam@gmail.com>
2019-10-03 20:46:33 -0400

    validator privatizer check
    helper functions for retrieving released nodes, sources and sinks, and to make sure there are no released private DataSources. Just a scrappy initial check. Would be better to traverse from the sources. Wiil probably switch to boost's graph lib

--- base/include/differential_privacy/graph.hpp
+++ base/include/differential_privacy/graph.hpp
@@ -1,9 +1,20 @@
 #ifndef OPENDP_LIBRARY_GRAPH_HPP
 #define OPENDP_LIBRARY_GRAPH_HPP
 
+#include <cstddef>
+#include <analysis.pb.h>
+
 extern "C" {
-    signed int validate_analysis(char* analysisBuffer);
-    void hello_world_validator();
+    signed int validateAnalysis(char* analysisBuffer, size_t length);
 }
+
+std::set<unsigned int> getSinks(const Analysis& analysis);
+std::set<unsigned int> getSources(const Analysis& analysis);
+std::set<unsigned int> getReleaseNodes (Analysis analysis);
+bool isPrivatizer(const Component& component);
+
+bool checkAllPathsPrivatized(const Analysis& analysis);
+template<class Set1, class Set2>
+bool is_disjoint(const Set1 &set1, const Set2 &set2);
 
 #endif //OPENDP_LIBRARY_GRAPH_HPP
--- base/src/graph.cpp
+++ base/src/graph.cpp
@@ -2,22 +2,95 @@
 
 #include "analysis.pb.h"
 #include <iostream>
+#include <queue>
 
 // Uncomment to force error if protobuf versions mismatch
 //GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-signed int validate_analysis(char* analysisBuffer) {
+signed int validateAnalysis(char* analysisBuffer, size_t length) {
 
-    std::string analysisString(analysisBuffer);
+    std::string analysisString(analysisBuffer, length);
     Analysis analysis;
     analysis.ParseFromString(analysisString);
 
-    std::cout << analysis.DebugString();
+    bool validity = true;
+    if (!checkAllPathsPrivatized(analysis)) validity = false;
 
     google::protobuf::ShutdownProtobufLibrary();
-    return true;
+    return validity;
 }
 
-void hello_world_validator () {
-    std::cout << "Hello World!" << std::endl;
+std::set<unsigned int> getSinks(const Analysis& analysis) {
+    auto nodeIds = std::set<unsigned int>();
+    for (const std::pair<unsigned int, Component>& nodePair : analysis.graph())
+        nodeIds.insert(nodePair.first);
+
+    for (const std::pair<unsigned int, Component>& node : analysis.graph())
+        for (const std::pair<std::string, unsigned int>& argument : node.second.arguments())
+            nodeIds.erase(argument.second);
+
+    return nodeIds;
+}
+
+std::set<unsigned int> getSources(const Analysis& analysis) {
+    auto nodeIds = std::set<unsigned int>();
+    for (const std::pair<unsigned int, Component>& nodePair : analysis.graph()) {
+        if (nodePair.second.arguments_size() > 0) continue;
+        nodeIds.insert(nodePair.first);
+    }
+    return nodeIds;
+}
+
+std::set<unsigned int> getReleaseNodes(Analysis analysis) {
+
+    std::set<unsigned int> releaseNodeIds;
+    auto sinkIds = getSinks(analysis);
+    std::queue<unsigned int, std::deque<unsigned int>> nodeQueue(
+            std::deque<unsigned int>(sinkIds.begin(), sinkIds.end()));
+
+    auto graph = *analysis.mutable_graph();
+    while (!nodeQueue.empty()) {
+        unsigned int nodeId = nodeQueue.front();
+        nodeQueue.pop();
+
+        Component component = graph[nodeId];
+        if (isPrivatizer(component))
+            releaseNodeIds.insert(nodeId);
+        else
+            for (const auto& argumentPair : component.arguments())
+                nodeQueue.push(argumentPair.second);
+    }
+
+    return releaseNodeIds;
+}
+
+bool isPrivatizer(const Component& component) {
+    if (component.has_mean()) return true;
+    return false;
+}
+
+bool checkAllPathsPrivatized(const Analysis& analysis) {
+    auto releaseNodes = getReleaseNodes(analysis);
+    auto sourceNodes = getSources(analysis);
+
+    return is_disjoint(releaseNodes, sourceNodes);
+}
+
+// adapted from https://stackoverflow.com/a/1964252/10221612
+template<class Set1, class Set2>
+bool is_disjoint(const Set1 &set1, const Set2 &set2) {
+    if (set1.empty() || set2.empty()) return true;
+
+    auto it1 = set1.begin(), it1End = set1.end();
+    auto it2 = set2.begin(), it2End = set2.end();
+
+    if (*it1 > *set2.rbegin() || *it2 > *set1.rbegin()) return true;
+
+    while (it1 != it1End && it2 != it2End) {
+        if (*it1 == *it2) return false;
+        if (*it1 < *it2) it1++;
+        else it2++;
+    }
+
+    return true;
 }
