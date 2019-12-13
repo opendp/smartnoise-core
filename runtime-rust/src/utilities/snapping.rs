34c087ee9401165818ddc22417b8cc8467ce877a
Christian Covington <christian.t.covington@gmail.com>
2019-12-13 14:14:16 -0500

    add f64 <=> binary mappings

--- runtime-rust/src/utilities/snapping.rs
+++ runtime-rust/src/utilities/snapping.rs
@@ -11,6 +11,8 @@ pub fn get_bytes(n_bytes: usize) -> String {
     /// # Arguments
     /// * `n_bytes` - A numeric variable
     ///
+    /// # Return
+    /// * `binary_string` - String of n_bytes bytes
 
     // read random bytes from OpenSSL
     let mut buffer = vec!(0_u8; n_bytes);
@@ -59,4 +61,59 @@ pub fn get_geom_prob_one_half() -> i16 {
         geom = cmp::min(geom, first_one_overall_index+1);
     }
     return geom;
+}
+
+pub fn f64_to_binary(num: f64) -> String {
+    /// Converts f64 to String of length 64, yielding the IEEE-754 binary representation of the number
+    ///
+    /// # Arguments
+    /// * `num` - a number of type f64
+    ///
+    /// # Return
+    /// * `binary_string`: String showing IEEE-754 binary representation of `num`
+
+
+    // decompose num into component parts
+    let (sign, exponent, mantissa) = num.decompose_raw();
+
+    // convert each component into strings
+    let sign_string = (sign as i64).to_string();
+    let mantissa_string = format!("{:052b}", mantissa);
+    let exponent_string = format!("{:011b}", exponent);
+
+    // join component strings
+    let binary_string = vec![sign_string, exponent_string, mantissa_string].join("");
+
+    // return string representation
+    return binary_string;
+}
+
+pub fn binary_to_f64(binary_string: String) -> f64 {
+    /// Converts String of length 64 to f64, yielding the floating-point number represented by the String
+    ///
+    /// # Arguments
+    /// * `binary_string`: String showing IEEE-754 binary representation of a number
+    ///
+    /// # Return
+    /// * `num`: f64 version of the String
+
+    // get sign and convert to bool as recompose expects
+    let sign = &binary_string[0..1];
+    let sign_bool = if sign.parse::<i32>().unwrap() == 0 {
+                        false
+                    } else {
+                        true
+                    };
+
+    // convert exponent to int
+    let exponent = &binary_string[1..12];
+    let exponent_int = u16::from_str_radix(exponent, 2).unwrap();
+
+    // convert mantissa to int
+    let mantissa = &binary_string[12..];
+    let mantissa_int = u64::from_str_radix(mantissa, 2).unwrap();
+
+    // combine elements into f64 and return
+    let num = f64::recompose_raw(sign_bool, exponent_int, mantissa_int);
+    return num;
 }
