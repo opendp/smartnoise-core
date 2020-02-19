4f28c134d817ed8ecf5d56b38110725302dbfca8
Shoeboxam <shoeboxam@gmail.com>
2020-02-19 02:15:40 -0500

    validator components module

--- validator-rust/src/components/row_wise_min.rs
+++ validator-rust/src/components/row_wise_min.rs
@@ -1,58 +1,11 @@
 use std::collections::HashMap;
 use crate::base::Constraint as Constraint;
+
+use crate::base;
 use crate::proto;
 use crate::hashmap;
 
-pub trait Component {
-    // modify min, max, n, categories, is_public, non-null, etc. based on the arguments and component
-    fn propagate_constraint<T>(
-        &self,
-        constraints: &HashMap<String, Constraint<T>>,
-    ) -> Constraint<T>;
-
-    fn is_valid<T>(
-        &self,
-        constraints: &HashMap<String, Constraint<T>>,
-    ) -> bool;
-
-    // return a hashmap of an expanded subgraph
-    fn expand<T>(
-        &self,
-        privacy_definition: &proto::PrivacyDefinition,
-        component: &proto::Component,
-        maximum_id: i32,
-        component_id: i32,
-        constraints: &HashMap<String, Constraint<T>>,
-    ) -> (i32, HashMap<i32, proto::Component>);
-
-    // compute the sensitivity
-    fn sensitivity<T>(
-        &self,
-        privacy_definition: &proto::PrivacyDefinition,
-        constraint: &Constraint<T>,
-    ) -> Option<f64>;
-
-    fn accuracy_to_privacy_usage<T>(
-        &self,
-        privacy_definition: &proto::PrivacyDefinition,
-        constraint: &Constraint<T>,
-        accuracy: &proto::Accuracy,
-    ) -> Option<proto::PrivacyUsage>;
-
-    fn privacy_usage_to_accuracy<T>(
-        &self,
-        privacy_definition: &proto::PrivacyDefinition,
-        constraints: &HashMap<String, Constraint<T>>,
-    ) -> Option<f64>;
-
-    // for json construction. Return type should be a generic serializable struct, not a String
-    fn summarize<T>(
-        &self,
-        constraints: &HashMap<String, Constraint<T>>,
-    ) -> String;
-}
-
-impl Component for proto::RowMin {
+impl base::Component for proto::RowMin {
     // modify min, max, n, categories, is_public, non-null, etc. based on the arguments and component
     fn propagate_constraint<T>(
         &self,
