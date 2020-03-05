1f976d856712db4f5abc4994ed9e3d7c6303f03b
Raman Prasad <raman_prasad@harvard.edu>
2020-03-05 12:11:58 -0500

    minor updates

--- bindings-python/debug.sh
+++ bindings-python/debug.sh
@@ -7,11 +7,13 @@ export RUST_BACKTRACE=1
 
 # rebuilds the validator, runtime, protobuf, components.py and python package
 #python3 setup.py develop |& tee debug_build.log
-python3 -m pip install -e . |& tee debug_build.log
+#python3 -m pip install -e . |& tee debug_build.log # ubuntu
+python3 -m pip install -e . | tee debug_build.log # Mac
 # run tests
 #python3 -m pytest -x -v |& tee debug_tests.log
 
 # run a test application
-python3 app.py |& tee debug_app.log
+#python3 app.py |& tee debug_app.log # ubuntu
+python3 app.py | tee debug_app.log   # Mac
 
 exit
--- validator-rust/README.md
+++ validator-rust/README.md
@@ -1,12 +1,10 @@
 
 1. install Rust
 
-2. turn on nightly channel
-
-        rustup override set nightly
-
 2. run the command (every code change)
-        
-        cargo +nightly build --release
+
+  - `cargo build`
+  - Alternatively build an optimized version (longer compilation/quick runtime):
+    - `cargo build --release`
 
 NOTE: Do not modify api.h. The file is automatically generated.
