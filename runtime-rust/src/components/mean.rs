2710d647c749ed546b11f37e68332a780604f67d
Christian Covington <christian.t.covington@gmail.com>
2020-03-17 19:19:35 -0400

    add mean documentation

--- runtime-rust/src/components/mean.rs
+++ runtime-rust/src/components/mean.rs
@@ -8,18 +8,41 @@ use crate::utilities::utilities::get_num_columns;
 use yarrow_validator::proto;
 
 impl Evaluable for proto::Mean {
+    /// Calculates the arithmetic mean of each column in the provided data.
+    ///
+    /// # Arguments
+    /// * `data` - Data for which you want the mean.
+    ///
+    /// # Return
+    /// Arithmetic mean(s) of the data in question.
     fn evaluate(&self, arguments: &NodeArguments) -> Result<Value> {
         Ok(mean(&get_argument(&arguments, "data")?.get_arraynd()?.get_f64()?.clone())?.into())
     }
 }
 
+/// Calculates the arithmetic mean of each column in the provided data.
+///
+/// # Arguments
+/// * `data` - Data for which you want the mean.
+///
+/// # Return
+/// Arithmetic mean(s) of the data in question.
+///
+/// # Example
+/// ```
+/// use ndarray::prelude::*;
+/// use yarrow_runtime::components::mean::mean;
+/// let data = arr2(&[ [1.,10.], [2., 20.], [3., 30.] ]).into_dyn();
+/// let means = mean(&data).unwrap();
+/// assert!(means[[0, 0]] == 2. && means[[0, 1]] == 20.);
+/// ```
 pub fn mean(data: &ArrayD<f64>) -> Result<ArrayD<f64>> {
-
     // iterate over the generalized columns
     let means = data.gencolumns().into_iter()
         .map(|column| column.mean()).collect::<Option<Vec<f64>>>()
         .ok_or::<Error>("attempted mean of an empty column".into())?;
 
+    // ensure means are of correct dimension
     let array = match data.ndim() {
         1 => Array::from_shape_vec(vec![], means),
         2 => Array::from_shape_vec(vec![1 as usize, get_num_columns(&data)? as usize], means),
