9ccdcf6d2d5e9001918a1e5a3497f0879b114546
saniavn <saniya.vahedian.movahed@gmail.com>
2020-03-17 20:51:35 -0400

    Documentation

--- validator-rust/src/utilities/json.rs
+++ validator-rust/src/utilities/json.rs
@@ -1,18 +1,32 @@
+<<<<<<< HEAD
 //! Representation for report/json summaries
+=======
+/// ***JSON Release*** ///
+>>>>>>> Documentation
 
 use crate::errors::*;
 use std::collections::{HashMap};
 use serde::{Deserialize, Serialize};
-
-//use schemars::{schema_for, JsonSchema};
-//extern crate json_typegen;
 extern crate serde_json;
+
 use crate::proto;
 use crate::base;
 
-//use json_typegen::json_typegen;
 use serde_json::Value;
 use ndarray::prelude::*;
+
+/// "struct JSONRelease" represents JSON objects.
+/// "description" is a string that describes the release.
+/// "variables" is an array of string that is column/s in the dataset.
+/// statistics is a string, it shows which statistic is implemented (e.g. mean, histogram, variance, etc.)
+/// "privacyLoss" is a Value type, Value is either an ArrayND, HashMap, or Vector2DJagged (for jagged matrices). User provide a value for either epsilon (epsilon>0), delta (0<delta<1>), or rho depending on the type of dp definitions (i.e. pure, approximated and concerted).
+/// "batch" is a numeric Which shows from which release the implemented statistic is originating from. This provides a tool to keep track of overall privacyLoss.
+/// "nodeID" is a numeric value, it shows the release corresponds to which node of the graph.
+/// "postprocess" is a Boolean, it is true when there are more than one variables.
+/// algorithmInfo returns the information about the algorithm and its parameters.
+/// "Accuracy" is an optional parameter. It is a combination of the accuracy and alpha value.
+/// "AlgorithmInfo" includes: the name of the algorithm which is implemented for computation of the given statistic and the arguments of the algorithm such as n(number of observations),  range (upper and lower bound, etc.)
+
 
 #[derive(Serialize, Deserialize)]
 pub struct JSONRelease {
@@ -28,11 +42,13 @@ pub struct JSONRelease {
     pub algorithmInfo: AlgorithmInfo,
 }
 
+
 #[derive(Serialize, Deserialize)]
 pub struct Accuracy {
     pub accuracyValue: f64,
     pub alpha: f64,
 }
+
 
 #[derive(Serialize, Deserialize)]
 pub struct AlgorithmInfo {
@@ -40,6 +56,10 @@ pub struct AlgorithmInfo {
     pub cite: String,
     pub argument: Value,
 }
+
+/// All components take in a HashMap<String, Value> and return a Value. A Value is either an ArrayND, HashMap, or Vector2DJagged (for jagged matrices).
+///A component is the smallest unit of computation in this framework. It represents some computation- Load a table (Materialize), extract a column from a table (Index), add two columns (Add), impute missing values (Impute), compute the mean of a column (Mean), add Laplacian noise, (LaplaceMechanism) etc.
+/// "value_to_json" function converts an ArrayND (which can take either of types (float, integer, string, and Boolean) to JSON via the “arraynd_to_json“  function.
 
 pub fn value_to_json(value: &base::Value) -> Result<serde_json::Value> {
     match value {
@@ -53,6 +73,10 @@ pub fn value_to_json(value: &base::Value) -> Result<serde_json::Value> {
     }
 }
 
+/// "arraynd_to_json" function converts Value to JSON. This function is required since the output of components is a Value not an ArrayND
+
+/// This function can be called  on arrays of any type "arrayD<T>" (T is a generic type for array).
+///converting n dimensional array to a vector, and converting to serde_json value which is the 2-dimensional value.
 pub fn arraynd_to_json<T: Serialize + Clone>(array: &ArrayD<T>) -> Result<serde_json::Value> {
     match array.ndim() {
         0 => Ok(serde_json::json!(array.first().unwrap())),
@@ -63,6 +87,8 @@ pub fn arraynd_to_json<T: Serialize + Clone>(array: &ArrayD<T>) -> Result<serde_
         _ => Err("converting a matrix to json is not implemented".into())
     }
 }
+
+/// "privacy_usage_to_json" returns PrivacyLoss Value. User provide a value for either epsilon, delta, or rho depending on the type of dp definitions (i.e. pure, approximated and concerted).
 
 pub fn privacy_usage_to_json(privacy_usage: &proto::PrivacyUsage) -> serde_json::Value {
     match privacy_usage.distance.clone().unwrap() {
