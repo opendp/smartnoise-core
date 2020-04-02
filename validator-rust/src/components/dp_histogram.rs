b8047f543f314823fa961510a3e956b5316a722d
Christian Covington <christian.t.covington@gmail.com>
2020-04-02 15:03:30 -0400

    midway testing

--- validator-rust/src/components/dp_histogram.rs
+++ validator-rust/src/components/dp_histogram.rs
@@ -6,11 +6,11 @@ use std::collections::HashMap;
 use crate::{proto, base};
 use crate::hashmap;
 use crate::components::{Component, Expandable, Report};
-
+use ndarray::{arr0};
 
 use crate::base::{NodeProperties, Value, ValueProperties};
 use crate::utilities::json::{JSONRelease, AlgorithmInfo, privacy_usage_to_json, value_to_json};
-use crate::utilities::{prepend, broadcast_privacy_usage, get_ith_release};
+use crate::utilities::{prepend, broadcast_privacy_usage, get_ith_release, get_literal};
 
 
 impl Component for proto::DpHistogram {
@@ -42,11 +42,36 @@ impl Expandable for proto::DpHistogram {
         component_id: &u32,
         maximum_id: &u32,
     ) -> Result<proto::ComponentExpansion> {
-        let mut current_id = *maximum_id;
+        let mut maximum_id = *maximum_id;
         let mut computation_graph: HashMap<u32, proto::Component> = HashMap::new();
+        let mut releases: HashMap<u32, proto::ReleaseNode> = HashMap::new();
 
         let mut data_id = component.arguments.get("data")
             .ok_or_else(|| Error::from("data is a required argument to DPHistogram"))?.to_owned();
+
+        let data_property = properties.get("data")
+                                        .ok_or("data: missing")?.array()
+                                        .map_err(prepend("data:"))?;
+
+        let count_max_id = match component.arguments.get("count_max") {
+            Some(id) => id.clone(),
+            None => {
+                let count_max = match data_property.num_records {
+                    Some(num_records) => arr0(num_records).into_dyn(),
+                    None => match self.enforce_constant_time {
+                        true => return Err("count_max must be set when enforcing constant time".into()),
+                        false => arr0(std::i64::MAX).into_dyn()
+                    }
+                };
+                // count_max
+                maximum_id += 1;
+                let id_count_max = maximum_id;
+                let (patch_node, count_max_release) = get_literal(&count_max.into(), &component.batch)?;
+                computation_graph.insert(id_count_max.clone(), patch_node);
+                releases.insert(id_count_max.clone(), count_max_release);
+                id_count_max
+            }
+        };
 
         let mut traversal = Vec::<u32>::new();
         match (component.arguments.get("edges"), component.arguments.get("categories")) {
@@ -57,8 +82,8 @@ impl Expandable for proto::DpHistogram {
                     .ok_or_else(|| Error::from("null is a required argument to DPHistogram"))?;
                 let inclusive_left_id = component.arguments.get("inclusive_left")
                     .ok_or_else(|| Error::from("inclusive_left is a required argument to DPHistogram when categories are not known"))?;
-                current_id += 1;
-                let id_digitize = current_id;
+                maximum_id += 1;
+                let id_digitize = maximum_id;
                 computation_graph.insert(id_digitize, proto::Component {
                     arguments: hashmap![
                         "data".to_owned() => data_id,
@@ -78,8 +103,8 @@ impl Expandable for proto::DpHistogram {
                 // clamp
                 let null_id = component.arguments.get("null")
                     .ok_or_else(|| Error::from("null is a required argument to DPHistogram when categories are not known"))?;
-                current_id += 1;
-                let id_clamp = current_id;
+                maximum_id += 1;
+                let id_clamp = maximum_id;
                 computation_graph.insert(id_clamp, proto::Component {
                     arguments: hashmap![
                         "data".to_owned() => data_id,
@@ -107,8 +132,8 @@ impl Expandable for proto::DpHistogram {
         }
 
         // histogram
-        current_id += 1;
-        let id_histogram = current_id;
+        maximum_id += 1;
+        let id_histogram = maximum_id;
         computation_graph.insert(id_histogram, proto::Component {
             arguments: hashmap!["data".to_owned() => data_id],
             variant: Some(proto::component::Variant::from(proto::Histogram {})),
@@ -123,8 +148,7 @@ impl Expandable for proto::DpHistogram {
                 "data".to_owned() => id_histogram,
                 "count_min".to_owned() => *component.arguments.get("count_min")
                     .ok_or_else(|| Error::from("count_min must be provided as an argument"))?,
-                "count_max".to_owned() => *component.arguments.get("count_max")
-                    .ok_or_else(|| Error::from("count_max must be provided as an argument"))?
+                "count_max".to_owned() => count_max_id
             ],
             variant: Some(proto::component::Variant::from(proto::SimpleGeometricMechanism {
                 privacy_usage: self.privacy_usage.clone(),
@@ -137,7 +161,7 @@ impl Expandable for proto::DpHistogram {
         Ok(proto::ComponentExpansion {
             computation_graph,
             properties: HashMap::new(),
-            releases: HashMap::new(),
+            releases,
             traversal
         })
     }
