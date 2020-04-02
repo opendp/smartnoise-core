41ed2d0a560da410b447a7498a9e82b2985f5f6d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-02 14:49:07 +0000

    Deploy opendifferentialprivacy/whitenoise-core to github.com/opendifferentialprivacy/whitenoise-core.git:gh-pages

--- doc/whitenoise_runtime/base/sidebar-items.js
+++ doc/whitenoise_runtime/base/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["execute_graph","Given a description of computation, and some computed values, execute the computation and return computed values"],["get_sinks","Retrieve the set of node ids in a ComputationGraph that have no dependent nodes."]],"type":[["NodeArguments",""]]});
+initSidebarItems({"fn":[["execute_graph","Given a description of computation, and some computed values, execute the computation and return computed values"],["get_sinks","Retrieve the set of node ids in a graph that have no dependent nodes."],["is_public",""]],"type":[["NodeArguments",""]]});
