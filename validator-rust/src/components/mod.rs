a4757305cc3df0da56b28923e58778d6606ed40c
Shoeboxam <shoeboxam@gmail.com>
2020-04-02 17:12:39 -0400

    consolidate Literal Named implementation

--- validator-rust/src/components/mod.rs
+++ validator-rust/src/components/mod.rs
@@ -457,37 +457,35 @@ impl Named for proto::Literal {
         _argument_variables: &HashMap<String, Vec<String>>,
         release: &Option<&Value>
     ) -> Result<Vec<String>> {
+
+        fn array_to_names<T: ToString + Clone + Default>(array: &ArrayD<T>, num_columns: i64) -> Result<Vec<String>> {
+            (0..num_columns as usize)
+                .map(|index| {
+                    let array = get_ith_release(array, &index)?;
+                    match array.ndim() {
+                        0 => match array.first() {
+                            Some(value) => Ok(value.to_string()),
+                            None => Err("array may not be empty".into())
+                        },
+                        1 => Ok("[Literal Column]".into()),
+                        _ => Err("array has too great of a dimension".into())
+                    }
+                })
+                .collect::<Result<Vec<String>>>()
+        }
+
         match release {
             Some(release) => match release {
                 Value::Jagged(jagged) => Ok((0..jagged.num_columns()).map(|_| "[Literal vector]".to_string()).collect()),
                 Value::Hashmap(_) => Err("names for hashmap literals are not supported".into()),  // (or necessary)
                 Value::Array(value) => match value {
-                    Array::F64(array) => (0..value.num_columns()? as usize)
-                        .map(|index| ndarray_to_name(get_ith_release(array, &index)?))
-                        .collect::<Result<Vec<String>>>(),
-                    Array::I64(array) => (0..value.num_columns()? as usize)
-                        .map(|index| ndarray_to_name(get_ith_release(array, &index)?))
-                        .collect::<Result<Vec<String>>>(),
-                    Array::Bool(array) => (0..value.num_columns()? as usize)
-                        .map(|index| ndarray_to_name(get_ith_release(array, &index)?))
-                        .collect::<Result<Vec<String>>>(),
-                    Array::Str(array) => (0..value.num_columns()? as usize)
-                        .map(|index| ndarray_to_name(get_ith_release(array, &index)?))
-                        .collect::<Result<Vec<String>>>()
+                    Array::F64(array) => array_to_names(array, value.num_columns()?),
+                    Array::I64(array) => array_to_names(array, value.num_columns()?),
+                    Array::Str(array) => array_to_names(array, value.num_columns()?),
+                    Array::Bool(array) => array_to_names(array, value.num_columns()?),
                 }
             },
             None => Err("Literals must always be accompanied by a release".into())
         }
-    }
-}
-
-fn ndarray_to_name<T: ToString>(array: ArrayD<T>) -> Result<String> {
-    match array.ndim() {
-        0 => match array.first() {
-            Some(value) => Ok(value.to_string()),
-            None => Err("array may not be empty".into())
-        },
-        1 => Ok("[Literal Column]".into()),
-        _ => Err("array has too great of a dimension".into())
     }
 }
