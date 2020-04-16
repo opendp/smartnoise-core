5670fb007f64db8e756c3a676be4dc538799659d
Shoeboxam <shoeboxam@gmail.com>
2020-04-16 03:42:00 -0400

    remove dead C++ files
    amazing that these are still here, I never noticed them because they were sitting outside of my IDE's file trees

--- dependencies.sh
+++
@@ -1,8 +0,0 @@
-# (re)installs dependencies using conan package manager
-
-for project in validator-c++ tests runtime-eigen;
-do
-  cd $project/
-  mkdir -p build && cd build && conan install .. && cd ..
-  cd ..
-done
