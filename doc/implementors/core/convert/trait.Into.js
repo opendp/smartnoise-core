6ce8e96ab276fe51bd17beb2605825eeab57ec9b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-07 15:06:21 +0000

    Deploy opendifferentialprivacy/smartnoise-core to github.com/opendifferentialprivacy/smartnoise-core.git:gh-pages

--- doc/implementors/core/convert/trait.Into.js
+++ doc/implementors/core/convert/trait.Into.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["backtrace"] = [{"text":"impl Into&lt;Vec&lt;BacktraceFrame&gt;&gt; for Backtrace","synthetic":false,"types":[]}];
+implementors["backtrace"] = [{"text":"impl Into&lt;Vec&lt;BacktraceFrame, Global&gt;&gt; for Backtrace","synthetic":false,"types":[]}];
 implementors["either"] = [{"text":"impl&lt;L, R&gt; Into&lt;Result&lt;R, L&gt;&gt; for Either&lt;L, R&gt;","synthetic":false,"types":[]}];
 implementors["gimli"] = [{"text":"impl Into&lt;u64&gt; for Pointer","synthetic":false,"types":[]},{"text":"impl&lt;'input, Endian&gt; Into&lt;&amp;'input [u8]&gt; for EndianSlice&lt;'input, Endian&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;Endian: Endianity,&nbsp;</span>","synthetic":false,"types":[]}];
 implementors["itertools"] = [{"text":"impl&lt;A, B&gt; Into&lt;Option&lt;Either&lt;A, B&gt;&gt;&gt; for EitherOrBoth&lt;A, B&gt;","synthetic":false,"types":[]}];
